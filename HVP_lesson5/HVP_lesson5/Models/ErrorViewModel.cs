f3ebd680854e8fa0b5f67e02d3b7eee7c29f8525
PhucTAIYO <sadlierlife2001@gmail.com>
2026-09-05 12:14:03 +0700

    HVPlesson5

---
+++ HVP_lesson5/HVP_lesson5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HVP_lesson5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
